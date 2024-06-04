4848cf943365137e53288b8627ed0e6e4947ae9b
admin <admin@192.168.0.3>
2024-06-05 00:21:15 +0500

    Add workflow with PEP8 linting

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -2,9 +2,11 @@
 from http import HTTPStatus
 
 from api import models
+
 from django.test import Client, TestCase
 
 class TaskiAPITestCase(TestCase):
+
     def setUp(self):
         self.guest_client = Client()
 
